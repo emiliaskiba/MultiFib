a93a08af5b80273a9f1538d54f3f530b490c4bb3
emiliaskiba <84234350+emiliaskiba@users.noreply.github.com>
2023-01-30 16:43:36 +0100

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      1 MultiFib <br/>
+      2 Emilia Skiba <br/>
+      3 I2S 2.4 <br/>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
